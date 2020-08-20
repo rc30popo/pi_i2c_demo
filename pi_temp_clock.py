55b92e57097d10ef6f7451de23be73e64b0ab4cf
Toy Fujii <r.t.19891111@gmail.com>
2020-08-20 20:55:36 +0900

    pi_temp_clcok.py:測定データCSVファイル保存対応

--- pi_temp_clock.py
+++ pi_temp_clock.py
@@ -4,15 +4,23 @@
 #    キャラクタ液晶 ACM1602と気圧センサーLPS25Hを利用したデモ
 #
 #  Rev.0.3 2020/08/17 SIGTERMで終了する様実装(バックグラウンド実行:nohup対応)
+#  Rev.0.4 2020/08/19 記録データのCSVファイル出力追加
 
 import time,smbus,sys,datetime
 import signal
+import argparse
+
 from ctrl_acm1602 import ctrl_acm1602,ctrl_acm1602_error
 from ctrl_lps25h import ctrl_lps25h,ctrl_lps25h_error
 
 # 終了処理
 def finish(message):
+    global acm1602
+    global lps25h
+    global output_f
     signal.setitimer(signal.ITIMER_REAL,0)
+    if output_f != None:
+        output_f.close()
     acm1602.clear_display()
     acm1602.write_string('Terminated!!')
     print(message)
@@ -30,6 +38,10 @@ def sigtermHandler(signum,frame):
 def intervalHandler(signum,frame):
     global acm1602
     global lps25h
+    global output_f
+    global rec_interval_cnt
+    global rec_interval
+
     acm1602.return_home()
     cur_press = lps25h.get_pressure()
     cur_temp = lps25h.get_temp()
@@ -39,7 +51,43 @@ def intervalHandler(signum,frame):
     acm1602.write_string(time_str)
     acm1602.set_cursor(0,1)
     acm1602.write_string(press_temp_str)
+    if output_f != None: # CSVファイル出力
+        if rec_interval_cnt == 0:
+            time_str2 = dt_now.strftime('%Y-%m-%d %H:%M:%S')
+            csv_str = '%s,%.1f,%.1f\n' % (time_str2,cur_temp,cur_press)
+            output_f.write(csv_str)
+        rec_interval_cnt += 1
+        if rec_interval_cnt == rec_interval:
+            rec_interval_cnt = 0
 
+
+# デフォルト引数
+rec_interval = 60 # レコードインターバル60秒
+output_file = None
+
+# レコードインターバル用カウンタ
+rec_interval_cnt = 0
+
+# 引数チェック
+parser = argparse.ArgumentParser(prog='pi_temp_clock.py',description='Temperature and Barometric pressure sensing program.')
+parser.add_argument('-o','--output',help='Output Filename',required=False)
+parser.add_argument('-r','--rec_interval',help='Record interval(sec)',type=int,required=False)
+
+args = parser.parse_args()
+
+if args.output != None:
+    output_file = args.output
+if args.rec_interval != None:
+    rec_interval = args.rec_interval
+
+print('==pi_temp_clock start==')
+if output_file != None:
+    print('Output File = ' + output_file)
+    print('Record Interval = %d(sec)' % rec_interval)
+
+
+
+# I2C初期化
 i2c_channel = 1 # Raspberry PI2に合わせて1,適宜修正の事
 i2c = smbus.SMBus(i2c_channel)
 
@@ -56,8 +104,21 @@ except ctrl_lps25h_error as e:
     print(e)
     exit(1)
 
+# 記録ファイルオープン
+output_f = None
+if output_file != None:
+    try:
+        output_f = open(output_file,'w')
+    except OSError as e: # ファイルオープンエラー
+        output_f = None # 念のため
+        print(e)
+    else: # ファイルオープン成功,ヘッダ書き込み
+        output_f.write('time,temp,baro\n')
+
+
 # 開始メッセージ
 acm1602.write_string('Activating..')
+
 # 各秒の開始から10ms以内を待ち
 while True:
     dt_now = datetime.datetime.now()
